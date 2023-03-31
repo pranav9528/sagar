c59f0d4ba7c2567b60fbbe2f5a2f22b4cc4fcc0b
Pranav <pranavkumarmtr>
2023-03-31 21:35:43 +0530

    thos is first commit

---
+++ repo1/android/app/src/main/kotlin/com/example/repo1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.repo1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ repo1/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegisterPage(),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({
+    super.key,
+  });
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  //TextEditingController nameController = TextEditingController();
+  // TextEditingController emailController = TextEditingController();
+  //extEditingController mobileController = TextEditingController();
+  //TextEditingController addressController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home),
+          ),
+          title: Text("registration "),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.info_outline),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert_outlined),
+            )
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: ListView(
+            children: const [
+              Text("heelo world"),
+              SizedBox(
+                height: 15,
+                //
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "enter firstname",
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("email"),
+              SizedBox(
+                height: 15,
+                //
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'enter name',
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("mobile"),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'enter mobile',
+                ),
+              ),
+              const Text("address"),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'enter addres ',
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
+        ));
+  }
+}
